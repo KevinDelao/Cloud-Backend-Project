e91628d907bbdf55e9977e395c0843f607f351ca
unknown <jsunthon@gmail.com>
2019-11-29 14:49:12 -0800

    fix up device [ci skip]

--- src/main/java/cs/csula/edu/cloudservice/dto/device/DevicePostDto.java
+++ src/main/java/cs/csula/edu/cloudservice/dto/device/DevicePostDto.java
@@ -4,11 +4,21 @@ public class DevicePostDto {
 
   private String name;
 
+  private String userId;
+
   public String getName() {
     return name;
   }
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
   }
 }
--- src/main/java/cs/csula/edu/cloudservice/service/DeviceService.java
+++ src/main/java/cs/csula/edu/cloudservice/service/DeviceService.java
@@ -3,25 +3,48 @@ package cs.csula.edu.cloudservice.service;
 
 import cs.csula.edu.cloudservice.dto.device.DevicePostDto;
 import cs.csula.edu.cloudservice.entity.device.Device;
+import cs.csula.edu.cloudservice.entity.user.User;
+import cs.csula.edu.cloudservice.exception.EntityNotProcessableException;
 import cs.csula.edu.cloudservice.repository.DeviceRepository;
+import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DeviceService {
 
+  private static final String USER_NOT_FOUND = "User with id %s does not exist.";
+
   private final DeviceRepository deviceRepository;
+  private final UserService userService;
   private final ModelMapper modelMapper;
 
-  public DeviceService(DeviceRepository deviceRepository, ModelMapper modelMapper) {
+  public DeviceService(DeviceRepository deviceRepository, UserService userService, ModelMapper modelMapper) {
     this.deviceRepository = deviceRepository;
+    this.userService = userService;
     this.modelMapper = modelMapper;
   }
 
   public Device createDevice(DevicePostDto devicePostDto) {
     Device device = modelMapper.map(devicePostDto, Device.class);
+    device.setUser(getUser(devicePostDto.getUserId()));
     return deviceRepository.save(device);
   }
 
+  private User getUser(String userId) {
+    UUID userUuid;
+    try {
+      userUuid = UUID.fromString(userId);
+    } catch (IllegalArgumentException ex) {
+      throw new EntityNotProcessableException(String.format(USER_NOT_FOUND, userId));
+    }
 
+    User user = userService.getUser(userUuid);
+
+    if (user == null) {
+      throw new EntityNotProcessableException(String.format(USER_NOT_FOUND, userId));
+    }
+
+    return user;
+  }
 }
