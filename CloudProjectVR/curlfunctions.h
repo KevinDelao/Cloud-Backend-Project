670d4d8dd4d439bd2fa7771c7fcd5b597acbae3f
Omar <ocojulu@calstatela.edu>
2020-02-21 17:49:45 -0800

    adding curl subfunctions

--- CloudProjectVR/curlfunctions.h
+++ CloudProjectVR/curlfunctions.h
@@ -1,1 +1,37 @@
-#pragma once
+#include <iostream>
+
+using namespace std;
+
+class User{
+private:
+	std::string userName;
+	std::string userNameHttpURL;
+public:
+		User() {
+		std::string userName = "";
+		std::string userNameHttpURL = "";
+	   }
+
+		User(std::string userName1, std::string userNameHttpURL2) {
+			userName = userName1;
+			userNameHttpURL = userNameHttpURL2;
+		}
+
+		std::string getUserName() {
+			return userName;
+		}
+
+		std::string getNameHttpURL(){
+			return userNameHttpURL;
+		}
+
+		void setUserName(std::string newName) {
+		userName = newName;
+		}
+
+		void setUserNameHttpURL(std::string newUserNameHttpURL) {
+		userNameHttpURL = newUserNameHttpURL;
+		}
+
+
+};
