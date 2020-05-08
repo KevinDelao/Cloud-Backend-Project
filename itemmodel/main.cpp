1e1108b51752302733e216415cc1ce7012c644eb
gnarlygypsy <56529086+gnarlygypsy@users.noreply.github.com>
2020-05-08 14:00:37 -0700

    Update main.cpp
    Added customization and comments for instruction

--- itemmodel/main.cpp
+++ itemmodel/main.cpp
@@ -26,6 +26,21 @@
 ** $QT_END_LICENSE$
 **
 ****************************************************************************/
+/**************************************************************************
+**Cloud-Backened-Project -  GUI development by Nicholas Martin 
+**The purpose of this Graphical User Interface is to allow for ease of data acquistion as well as data analysis
+**for the input of coordinate data sourced from the HTC Vive Virtual Reality Headset. The GUI shall provide the
+**following capabilities:
+**Real-time data feed of Coordinate data 
+**Populate real-time data into Table widget
+**Activate/Deactivate main code using button widgets
+**Save data files to specific file directories
+**Implement Graph widgets
+**
+**Due to events related to COVID-19 the development process had slowed rapidly. The currently
+**does not function completely, however, coupled wiht the documentation provided 
+**it shall serve as a satisfactoy baseline for future GUI development.
+***************************************************************************/
 
 #include <QtDataVisualization/q3dbars.h>
 #include <QtDataVisualization/qcategory3daxis.h>
@@ -39,16 +54,30 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QTableWidget>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QFileDialog>
 #include <QtGui/QScreen>
 #include <QtCore/QRandomGenerator>
 #include <QtCore/QTimer>
+#include <QtCore/QFile>
+#include <QtCore/QString>
 #include <QtGui/QFont>
 #include <QtCore/QDebug>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QMessageBox>
 
-#define USE_STATIC_DATA
 
+class QtWidgets : public QObject // Declare class type QtWidgets to get access to QSaveButton
+{
+ public:
+    explicit QtWidgets(QPushButton *save_button);
+    ~QtWidgets();
+
+    void QPushButton(); // Declare push button function to be used later for save button nimplementation
+};
+
+
+#define USE_STATIC_DATA
 using namespace QtDataVisualization;
 
 class GraphDataGenerator : public QObject
@@ -57,6 +86,7 @@ public:
     explicit GraphDataGenerator(Q3DBars *bargraph, QTableWidget *tableWidget);
     ~GraphDataGenerator();
 
+    // Declare all classes and functions needed throught the .cpp file
     void setupModel();
     void addRow();
     void changeStyle();
@@ -67,24 +97,26 @@ public:
     void selectedFromTable(int currentRow, int currentColumn, int previousRow, int previousColumn);
     void fixTableSize();
 
+
+
 private:
     Q3DBars *m_graph;
     QTimer *m_dataTimer;
     int m_columnCount;
     int m_rowCount;
     QTableWidget *m_tableWidget; // not owned
+
 };
 
 GraphDataGenerator::GraphDataGenerator(Q3DBars *bargraph, QTableWidget *tableWidget)
     : m_graph(bargraph),
       m_dataTimer(0),
-      m_columnCount(100),
-      m_rowCount(50),
+      m_columnCount(5),
+      m_rowCount(10),
       m_tableWidget(tableWidget)
 {
     //! [5]
-    // Set up bar specifications; make the bars as wide as they are deep,
-    // and add a small space between them
+
     m_graph->setBarThickness(1.0f);
     m_graph->setBarSpacing(QSizeF(0.2, 0.2));
 
@@ -113,13 +145,13 @@ GraphDataGenerator::GraphDataGenerator(Q3DBars *bargraph, QTableWidget *tableWid
 
     //! [7]
     // Set theme
-    m_graph->activeTheme()->setType(Q3DTheme::ThemeDigia);
+    m_graph->activeTheme()->setType(Q3DTheme::ThemeArmyBlue);
 
     // Set font
-    m_graph->activeTheme()->setFont(QFont("Impact", 20));
+    m_graph->activeTheme()->setFont(QFont("Helvetica", 20));
 
     // Set preset camera position
-    m_graph->scene()->activeCamera()->setCameraPreset(Q3DCamera::CameraPresetFront);
+    m_graph->scene()->activeCamera()->setCameraPreset(Q3DCamera::CameraPresetFront); // Eventually for real time camrea feed
     //! [7]
 }
 
@@ -154,30 +186,48 @@ void GraphDataGenerator::start()
 #endif
 }
 
+
+// Save Data to .csv file - must still incorporate save button after function is finished
+
+void QtWidgets::QPushButton()
+{
+QString filename = QFileDialog::getSaveFileName(QWidget *parent = nullptr, "DialogTitle", "filename.csv", "CSV files (.csv);;Zip files (.zip, *.7z)", 0, 0); // getting the filename (full path)
+QFile data(filename);
+
+if (data.open(QFile::WriteOnly|QFile::Truncate))
+{
+    QTextStream output(&data);
+    output << "'Data has been exported!'";
+}}
+// End of save button section
+
+
+
 void GraphDataGenerator::setupModel()
 {
     //! [9]
     // Set up row and column names
-    QStringList days;
-    days << "Monday" << "Tuesday" << "Wednesday" << "Thursday" << "Friday" << "Saturday" << "Sunday";
-    QStringList weeks;
-    weeks << "week 1" << "week 2" << "week 3" << "week 4" << "week 5";
+    QStringList metric;
+    metric << "X - Location" << "Y - Location" << "Z - Location" << "R - Rotation" << "X Theta" << "Y Theta" << "Z Theta";
+    QStringList TimeStamp;
+    TimeStamp << "1" << "2" << "3" << "4" << "5";
 
-    // Set up data         Mon  Tue  Wed  Thu  Fri  Sat  Sun
-    float hours[5][7] = {{2.0f, 1.0f, 3.0f, 0.2f, 1.0f, 5.0f, 10.0f},     // week 1
-                         {0.5f, 1.0f, 3.0f, 1.0f, 2.0f, 2.0f, 3.0f},      // week 2
-                         {1.0f, 1.0f, 2.0f, 1.0f, 4.0f, 4.0f, 4.0f},      // week 3
-                         {0.0f, 1.0f, 0.0f, 0.0f, 2.0f, 2.0f, 0.3f},      // week 4
-                         {3.0f, 3.0f, 6.0f, 2.0f, 2.0f, 1.0f, 1.0f}};     // week 5
+    // Hardcoded data for the sake of data display - must pass variable that contains actual data from HTC code
+    // Set up data         X     Y     Z     R    X Theta - Y Theta - Z Theta
+    float hours[5][7] = {{2.0f, 1.0f, 3.0f, 0.2f, 1.0f, 5.0f, 10.0f},     // 1
+                         {0.5f, 1.0f, 3.0f, 1.0f, 2.0f, 2.0f, 3.0f},      // 2
+                         {1.0f, 1.0f, 2.0f, 1.0f, 4.0f, 4.0f, 4.0f},      // 3
+                         {0.0f, 1.0f, 0.0f, 0.0f, 2.0f, 2.0f, 0.3f},      // 4
+                         {3.0f, 3.0f, 6.0f, 2.0f, 2.0f, 1.0f, 1.0f}};     // 5
     //! [9]
 
     // Add labels
     //! [10]
-    m_graph->rowAxis()->setTitle("Week of year");
+    m_graph->rowAxis()->setTitle("Week of Activity");
     m_graph->rowAxis()->setTitleVisible(true);
-    m_graph->columnAxis()->setTitle("Day of week");
+    m_graph->columnAxis()->setTitle("Coordinate Activity");
     m_graph->columnAxis()->setTitleVisible(true);
-    m_graph->valueAxis()->setTitle("Hours spent on the Internet");
+    m_graph->valueAxis()->setTitle("Average Activity");
     m_graph->valueAxis()->setTitleVisible(true);
     m_graph->valueAxis()->setLabelFormat("%.1f h");
     //! [10]
@@ -185,24 +235,27 @@ void GraphDataGenerator::setupModel()
     //! [11]
     m_tableWidget->setRowCount(5);
     m_tableWidget->setColumnCount(7);
-    m_tableWidget->setHorizontalHeaderLabels(days);
-    m_tableWidget->setVerticalHeaderLabels(weeks);
+    m_tableWidget->setHorizontalHeaderLabels(metric);
+    m_tableWidget->setVerticalHeaderLabels(TimeStamp);
     m_tableWidget->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     m_tableWidget->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     m_tableWidget->setCurrentCell(-1, -1);
     m_tableWidget->setSelectionMode(QAbstractItemView::SingleSelection);
     //! [11]
 
+
+    // Setting table size
     //! [12]
-    for (int week = 0; week < weeks.size(); week++) {
-        for (int day = 0; day < days.size(); day++) {
-            QModelIndex index = m_tableWidget->model()->index(week, day);
-            m_tableWidget->model()->setData(index, hours[week][day]);
+    for (int second = 0; second< TimeStamp.size(); second++) {
+        for (int day = 0; day < metric.size(); day++) {
+            QModelIndex index = m_tableWidget->model()->index(second, day);
+            m_tableWidget->model()->setData(index, hours[second][day]);
         }
     }
     //! [12]
 }
 
+// Dynamic data population for data table - for example data
 void GraphDataGenerator::addRow()
 {
     m_tableWidget->model()->insertRow(0);
@@ -276,16 +329,17 @@ int main(int argc, char **argv)
 
     tableWidget->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
     tableWidget->setAlternatingRowColors(true);
-    widget.setWindowTitle(QStringLiteral("Hours spent on the Internet"));
+    widget.setWindowTitle(QStringLiteral("Average Activity"));
 
     //! [2]
     // Since we are dealing with QTableWidget, the model will already have data sorted properly
-    // into rows and columns, so we simply set useModelCategories property to true to utilize this.
+    // into rows and columns, so set useModelCategories property to true to utilize this.
+
     QItemModelBarDataProxy *proxy = new QItemModelBarDataProxy(tableWidget->model());
     proxy->setUseModelCategories(true);
     QBar3DSeries *series = new QBar3DSeries(proxy);
-    series->setMesh(QAbstract3DSeries::MeshPyramid);
-    graph->addSeries(series);
+    series->setMesh(QAbstract3DSeries::MeshCube); // Look up QAbstract3DSeries for bar graph options
+    graph->addSeries(series); // Add Bar graph style to bar graph widget
     //! [2]
 
     //! [3]
